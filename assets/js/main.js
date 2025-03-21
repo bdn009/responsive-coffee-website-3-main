cd361d738165302070421d5d09a0814a223851a4
Ashikur Rahman <ashikurrahaman236@gmail.com>
2025-03-21 16:40:09 +0600

    nav done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,26 @@
 /*=============== SHOW MENU ===============*/
-
+const toggle=document.getElementById('nav-toggle');
+const navMenu=document.getElementById('nav-menu');
+const navClose=document.getElementById('nav-close');
+if(toggle){
+    toggle.addEventListener('click',()=>{
+        navMenu.classList.toggle('show__menu');
+    });
+}
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show__menu');
+    });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink=document.querySelectorAll('.nav__link');
 
+const linkAction=()=>{
+    const navMenu=document.getElementById('nav-menu');
+    navMenu.classList.remove('show__menu');
+}
+navLink.forEach(n=>n.addEventListener('click',linkAction));
 
 /*=============== ADD SHADOW HEADER ===============*/
 
